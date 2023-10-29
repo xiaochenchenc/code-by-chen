c272baa394fb4dbd3037bd9637da4a9a5a672042
xiaochenchenc <148531890+xiaochenchenc@users.noreply.github.com>
2023-10-29 20:56:44 +0800

    Update StepsTask1.c

--- C-CW1/StepsTask1.c
+++ C-CW1/StepsTask1.c
@@ -1,7 +1,18 @@
 int main() {
-    FILE* file;
-    char ch;
-    file = fopen("FitnessData_2023.csv", "r");
-    printf("%c", file);
-    return 0;
+    //Open the FitnessData_2023.csv File
+FILE* file;
+int ch;
+if (fopen_s(&file,"FitnessData_2023.csv","r")!= 0)
+{
+    printf("error\n");
+    return 1;
+}
+while ((ch = fgetc(file)) != EOF)
+{
+    putchar(ch);
+}
+fclose(file);
+
+
+return 0;
 }
